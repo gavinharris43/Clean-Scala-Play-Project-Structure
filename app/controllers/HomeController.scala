890f234f13d18a3dcffb706e9825cfeac603d86e
Gavin Harris <gavinharris43@googlemail.com>
2020-03-13 13:59:43 +0000

    play day one

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,16 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("hello"))
+  }
+  def hello(msg: String) = Action {
+    Ok(views.html.index(msg))
+  }
+  def hello1 = Action{
+    Ok(views.html.index("Hello World!!!"))
+  }
+  def name = Action{
+    Ok(views.html.name())
   }
 
 }
